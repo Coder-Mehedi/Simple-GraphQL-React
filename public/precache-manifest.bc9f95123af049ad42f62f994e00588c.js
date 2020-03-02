37100725001228a0b1467d1b01f83054bce1b8d6
Coder-Mehedi <mehedi5051@gmail.com>
2020-03-02 18:13:08 +0600

    Finished react and deploying

---
+++ public/precache-manifest.bc9f95123af049ad42f62f994e00588c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a74e9340c0e62aacdb35f2b1adff17a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c94c884c9e6edf76f41",
+    "url": "/static/css/main.a96ee777.chunk.css"
+  },
+  {
+    "revision": "f92ef22b301b9baa02c0",
+    "url": "/static/js/2.a8b20d5d.chunk.js"
+  },
+  {
+    "revision": "d371590fcabfcefa3eb0ec019be33df2",
+    "url": "/static/js/2.a8b20d5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c94c884c9e6edf76f41",
+    "url": "/static/js/main.53146e69.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
